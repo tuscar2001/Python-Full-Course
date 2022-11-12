2cee31d850680561f79f2c2318570a38f4a0324b
Tam Adimi <tuscar2001@gmail.com>
2022-11-12 12:53:07 -0800

    first commit

---
+++ 1 numbers.py
@@ -0,0 +1,34 @@
+#Python has core objects
+#1. Integers having no fractional part, 2. floating point numbers that have fractional parts
+#3. complex numbers with imaginary parts, 4. decimals with fixed precision, 5. rationals with numerator
+# and denominator
+
+#integer addition
+print (555 + 666)
+
+
+#floating point multiplication
+print (1.5 * 6)
+
+
+#powers
+print (3 ** 100)
+
+
+# be aware of displays
+3.14567 * 2
+print (3.14567 * 2)
+
+
+# there are some python built modules we can import
+import math
+print (math.pi)
+
+print (math.sqrt(24))
+
+
+
+#random number generation
+import random
+print (random.random())
+print (random.choice([10,11,23,45]))
---
+++ fStrings.py
@@ -0,0 +1,9 @@
+prenom = "Jon"
+nom = "Doe"
+
+print("Prenom: {}, Nom: {}".format(prenom, nom))
+
+print(f"Prenom:{prenom}, Nom:{nom}")
+
+mois= 8
+path = f"/home/{mois}/docs/"
